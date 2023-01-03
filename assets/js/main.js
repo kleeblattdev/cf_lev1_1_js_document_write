231f089cf6eba9ffcbc83cad31b875f9fea52873
karinlee <karinlee92@gmail.com>
2023-01-03 14:07:24 +0100

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+document.write("<h1> Hello World</h1>")
+document.write("<p> Have a nice day</p>")
+
+let myText = "Hello again<br>";
+let vorname = "Karin";
+let nachname = "Lee";
+
+document.write(myText);
+document.write("<br>");
+document.write("Name: "+ vorname);
+document.write("Nachname: " + nachname);
+document.write("<br>");
+
+// Unterschied write und writeln
+
+document.writeln(myText);
+document.writeln("<br>");
+document.writeln("Name: "+ vorname); // writeln adds a space to the next 
+document.writeln("Nachname: " + nachname);
